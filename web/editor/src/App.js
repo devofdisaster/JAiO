94871669412227fbb095c33c3586fd3afdfb5bd9
pwitko <piotr@landingi.com>
2017-05-30 19:52:20 +0200

    Get rid of create-react-app bullshit

--- web/editor/src/App.js
+++ web/editor/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class Application {
+    constructor () {
+
+    }
+
+    init() {
+        ReactDOM.render(
+            <div id="page">Page goes here</div>,
+            document.getElementById('root')
+        )
+    }
 }
 
-export default App;
+export default Application
