223a825a1c1cb1e50e5fc057bc6c115376267dc6
pwitko <piotr@landingi.com>
2017-06-02 20:45:49 +0200

    Add movable elements

--- web/editor/src/View/Page/Element/SectionView.js
+++ web/editor/src/View/Page/Element/SectionView.js
@@ -4,12 +4,42 @@ import ElementFrame from '../UI/ElementFrame'
 import React from 'react'
 
 class SectionView extends ElementComponent {
+
+    constructor (props) {
+        super(props)
+
+        this.elem = null
+    }
+
+    componentDidMount() {
+        $(this.elem).droppable({
+            accept: '.ui-draggable',
+            greedy: true,
+            drop: (e, ui) => {
+                const type = ui.draggable[0].getAttribute('type');
+
+                if (type) {
+                    window.Application.addNewElement(this.props.source.uuid, type, ui.offset)
+                } else {
+                    window.Application.moveElement(
+                        ui.draggable[0].getAttribute('id'),
+                        this.props.source.uuid,
+                        ui.offset
+                    )
+                }
+            }
+        })
+    }
+
     render() {
         const source = this.props.source
         const style = this.renderStyle(source.parameters.style)
         const children = this.renderKids(source.children)
 
-        return <div id={source.uuid} style={style} onClick={this.handleClick} className="element-section">
+        return <div id={source.uuid} style={style} onClick={this.handleClick}
+                    className="element-section"
+                    ref={(elem) => this.elem = elem}
+        >
             <ElementFrame visible={source.selected}/>
             {children}
         </div>
