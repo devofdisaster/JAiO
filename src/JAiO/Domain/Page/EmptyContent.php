e363fd87ea7d2fb08dac060f78a26f696eb578fc
pwitko <piotr@landingi.com>
2017-06-02 22:05:49 +0200

    Prepare text php infrastructure

--- src/JAiO/Domain/Page/EmptyContent.php
+++ src/JAiO/Domain/Page/EmptyContent.php
@@ -30,39 +30,7 @@ class EmptyContent implements \JsonSerializable
                             'position' => 'relative'
                         ]
                     ],
-                    'children' => [
-                        [
-                            'type' => 'box',
-                            'attributes' => [],
-                            'parameters' => [
-                                'style' => [
-                                    'background-color' => 'red',
-                                    'box-sizing' => 'border-box',
-                                    'width' => 100,
-                                    'height' => 100,
-                                    'top' => 340,
-                                    'left' => 234,
-                                    'position' => 'absolute'
-                                ]
-                            ]
-                        ],
-                        [
-                            'type' => 'box',
-                            'attributes' => [],
-                            'parameters' => [
-                                'style' => [
-                                    'background-color' => 'blue',
-                                    'border-radius' => 50,
-                                    'box-sizing' => 'border-box',
-                                    'width' => 100,
-                                    'height' => 100,
-                                    'top' => 243,
-                                    'left' => 456,
-                                    'position' => 'absolute'
-                                ]
-                            ]
-                        ]
-                    ]
+                    'children' => []
                 ]
             ]
         ];
