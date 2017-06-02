8dd6dd4b22fc6784943b4e9ea9b03d5c1c01f707
pwitko <piotr@landingi.com>
2017-06-02 14:36:28 +0200

    Remove unnecessary constructors

--- web/editor/src/View/Page/UI/ElementFrame.js
+++ web/editor/src/View/Page/UI/ElementFrame.js
@@ -1,13 +1,6 @@
 import React from 'react'
 
 class ElementFrame extends React.Component {
-
-    constructor (props) {
-        super(props)
-
-        this.props = props
-    }
-
     render() {
         const classes = `element-frame ${this.props.visible ? 'visible' : ''}`
 
