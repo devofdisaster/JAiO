b9ac0ce3118e83e6fbee0eb2ad4651538996268d
pwitko <piotr@landingi.com>
2017-06-02 19:03:40 +0200

    Add cloning action to factory and make sure we get new objects when calling toJSON() on elements

--- web/editor/src/Page/ElementFactory.js
+++ web/editor/src/Page/ElementFactory.js
@@ -1,6 +1,7 @@
 import Body from './Element/Body'
 import Box from './Element/Box'
 import Section from './Element/Section'
+import Uuid from 'uuid'
 
 const mapping = {
     'body': Body,
@@ -11,6 +12,14 @@ const mapping = {
 class ElementFactory {
     static buildElement(object) {
         if (mapping[object.type]) {
+            return new mapping[object.type](object)
+        }
+    }
+
+    static cloneElement(object) {
+        if (mapping[object.type]) {
+            object.uuid = Uuid();
+
             return new mapping[object.type](object)
         }
     }
